ad0f8d8d0e38072c5b3957abd0b51eb2dacaf55d
BarutanGNE <esudaburiyu@gmail.com>
2014-01-27 22:12:16 -0500

    from cloud9

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ var app = express();
 var server = http.createServer(app);
 var io = socket_io.listen(server);
 
-server.listen(3000);
+server.listen(process.env.PORT, process.env.IP);
 
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on("connection", function (socket) {
   // メッセージ送信（送信者にも送られる）
   socket.on("C_to_S_message", function (data) {
-    msg = validator.escape(data.value);
+    var msg = validator.escape(data.value);
     io.sockets.emit("S_to_C_message", {msg: msg, id: socket.id});
   });
   // 切断したときに送信
