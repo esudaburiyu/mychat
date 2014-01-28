23312ec68c37c9e747dc4a2f7f541aa991882e23
barutanGNE <esudaburiyu@gmail.com>
2014-01-28 10:26:12 +0900

    validator

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var express = require('express');
 var socket_io = require('socket.io');
+var validator = require('validator');
 
 var app = express();
 var server = http.createServer(app);
@@ -11,12 +12,11 @@ server.listen(3000);
 app.use(express.static(__dirname + '/public'));
 
 io.sockets.on("connection", function (socket) {
-
   // メッセージ送信（送信者にも送られる）
   socket.on("C_to_S_message", function (data) {
-    io.sockets.emit("S_to_C_message", {value:data.value, id:socket.id});
+    msg = validator.escape(data.value);
+    io.sockets.emit("S_to_C_message", {msg: msg, id: socket.id});
   });
-
   // 切断したときに送信
   socket.on("disconnect", function () {
 //    io.sockets.emit("S_to_C_message", {value:"user disconnected"});
